54005ea39fc118702587a21a740f422f3e13ad85
Paul K <paul.k.online@gmail.com>
2017-04-07 08:45:48 +0300

    refactoring Position

--- MPTLib/Model/Positions/Position.cs
+++ MPTLib/Model/Positions/Position.cs
@@ -5,26 +5,12 @@ namespace MPT.Model
     public abstract class Position : IPlcIdPosition
     {
         public int PlcId { get; set; }
-
         public int Number { get; set; }
-        
         public string Name { get; set; }
-
         public string Description { get; set; }
-
         public string Note { get; set; }
-
         public int? GroupId { get; set; }
         
-        
-        /// <summary>
-        /// полное название
-        /// </summary>
-        public string FullName
-        {
-            get { return string.Format("{0} - {1}", Name, Description); }
-        }
-
         public bool Equals(IPlcIdPosition other)
         {
             return ByPlcIdNumberPositionComparer.Comparer.Equals(this, other);
@@ -34,22 +20,5 @@ namespace MPT.Model
         {
             return Name;
         }
-
-        /// <summary>
-        /// сравнивать только по индексу
-        /// </summary>
-        public class ByIdComparer : IEqualityComparer<Position>
-        {
-            public bool Equals(Position x, Position y)
-            {
-                return x.PlcId == y.PlcId && x.Number == y.Number ;
-            }
-
-            public int GetHashCode(Position x)
-            {
-                return (x.PlcId ^ x.Number).GetHashCode();
-            }
-        }
-
     }
 }
