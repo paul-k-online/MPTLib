0b90fb3ad7e6dda6cc3c0f4c497cc2dba8f98dfa
Paul K <paul.k.online@gmail.com>
2017-04-07 09:06:14 +0300

    fix RSviewUserAccount

--- MPTLib.Test/RSView/UserAccount/RSViewUserAccountTests.cs
+++ MPTLib.Test/RSView/UserAccount/RSViewUserAccountTests.cs
@@ -5,14 +5,65 @@ using System.Text;
 using System.IO;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 using MPT.RSView.UserAccount;
+
 
 namespace MPT.RSView.Password.Tests
 {
     [TestClass()]
     public class RSViewUserAccountTests
     {
-        Encoding utf16 = Encoding.Unicode;
+        static List<RSViewUserAccount.PassHashPair> testPassHashList = new List<RSViewUserAccount.PassHashPair>()
+        {
+            new RSViewUserAccount.PassHashPair {
+                Password = "",
+                Hash = new byte[]
+                {
+                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69,
+                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69,
+                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69,
+                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69
+                } },
+            new RSViewUserAccount.PassHashPair {
+                Password = "1",
+                Hash = new byte[]
+                {
+                        0xdc, 0xe8, 0xc8, 0x1e, 0x64, 0x94, 0x9f, 0x4e,
+                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69,
+                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69,
+                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69
+                } },
+            new RSViewUserAccount.PassHashPair {
+                Password = "2",
+                Hash = new byte[]
+                {
+                        0x32, 0x51, 0xc6, 0x21, 0xd3, 0x1a, 0xfe, 0xdd,
+                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69,
+                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69,
+                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69,
+                } },
+            new RSViewUserAccount.PassHashPair {
+                Password = "1111",
+                Hash = new byte[]
+                {
+                        0x5f, 0x7d, 0x04, 0xcf, 0x53, 0x7c, 0x20, 0xbb,
+                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69,
+                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69,
+                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69
+                } },
+            new RSViewUserAccount.PassHashPair {
+                Password  = "11111",
+                Hash = new byte[]
+                {
+                        0x5f, 0x7d, 0x04, 0xcf, 0x53, 0x7c, 0x20, 0xbb,
+                        0xdc, 0xe8, 0xc8, 0x1e, 0x64, 0x94, 0x9f, 0x4e,
+                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69,
+                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69,
+                } },
+        };
+
+
         [TestMethod()]
         public void UTF16Test()
         {
@@ -25,20 +76,21 @@ namespace MPT.RSView.Password.Tests
                 0x55, 0,
                 0x4C, 0,
                 0x54, 0,};
-            var str_decode = utf16.GetString(str_code);
+            var str_decode = Encoding.Unicode.GetString(str_code);
             Assert.AreEqual(str_decode, str);
         }
+
 
         [TestMethod()]
         public void DeserealizeTest()
         {
             var defaultAccount =
-                RSViewUserAccount.Deserealize(File.ReadAllBytes(@"RSView\UserAccount\Data\Users\Default.bin"));
+                RSViewUserAccount.Deserealize(File.ReadAllBytes(@"_TestData\RSView\Users\Default.bin"));
             Assert.AreEqual(defaultAccount.AccountId, "DEFAULT");
 
             {
             var account =
-                RSViewUserAccount.Deserealize(File.ReadAllBytes(@"RSView\UserAccount\Data\Users\LoginMacro.bin"));
+                RSViewUserAccount.Deserealize(File.ReadAllBytes(@"_TestData\RSView\Users\LoginMacro.bin"));
             Assert.AreEqual(account.AccountId, "MACRO1");
             Assert.AreEqual(account.LoginMacro, "MACRO");
             Assert.IsNull(account.LogoutMacro);
@@ -46,7 +98,7 @@ namespace MPT.RSView.Password.Tests
 
             {
                 var account =
-                    RSViewUserAccount.Deserealize(File.ReadAllBytes(@"RSView\UserAccount\Data\Users\LogoutMacro.bin"));
+                    RSViewUserAccount.Deserealize(File.ReadAllBytes(@"_TestData\RSView\Users\LogoutMacro.bin"));
                 Assert.AreEqual(account.AccountId, "MACRO2");
                 Assert.IsNull(account.LoginMacro);
                 Assert.AreEqual(account.LogoutMacro, "MACRO");
@@ -54,7 +106,7 @@ namespace MPT.RSView.Password.Tests
 
             {
                 var account =
-                    RSViewUserAccount.Deserealize(File.ReadAllBytes(@"RSView\UserAccount\Data\Users\Password.bin"));
+                    RSViewUserAccount.Deserealize(File.ReadAllBytes(@"_TestData\RSView\Users\Password.bin"));
                 Assert.AreEqual(account.AccountId, "PASSWORD");
                 Assert.IsNull(account.LoginMacro, null);
                 Assert.AreEqual(account.LogoutMacro, null);
@@ -62,7 +114,7 @@ namespace MPT.RSView.Password.Tests
             /*
             {
                 var account =
-                    RSViewUserAccount.Deserealize(File.ReadAllBytes(@"RSView\UserAccount\Data\Users\АБВГДЕЖЗ.bin"));
+                    RSViewUserAccount.Deserealize(File.ReadAllBytes(@"_TestData\RSView\Users\АБВГДЕЖЗ.bin"));
                 Assert.AreEqual(account.AccountId, "АБВГДЕЖЗ");
                 Assert.AreEqual(account.LoginMacro, "");
                 Assert.AreEqual(account.LogoutMacro, "");
@@ -70,12 +122,13 @@ namespace MPT.RSView.Password.Tests
             */
         }
 
+
         [TestMethod]
         public void PasswordTest()
         {
-            var account1x4 = RSViewUserAccount.Deserealize(File.ReadAllBytes(@"RSView\UserAccount\Data\Users\Passwords\1111.bin"));
-            var account1x8 = RSViewUserAccount.Deserealize(File.ReadAllBytes(@"RSView\UserAccount\Data\Users\Passwords\11111111.bin"));
-            var account1234 = RSViewUserAccount.Deserealize(File.ReadAllBytes(@"RSView\UserAccount\Data\Users\Passwords\1111222233334444.bin"));
+            var account1x4 = RSViewUserAccount.Deserealize(File.ReadAllBytes(@"_TestData\RSView\Users\Passwords\1111.bin"));
+            var account1x8 = RSViewUserAccount.Deserealize(File.ReadAllBytes(@"_TestData\RSView\Users\Passwords\11111111.bin"));
+            var account1234 = RSViewUserAccount.Deserealize(File.ReadAllBytes(@"_TestData\RSView\Users\Passwords\1111222233334444.bin"));
 
 
         }
--- MPTLib/RSView/UserAccount/RSViewUserAccount.cs
+++ MPTLib/RSView/UserAccount/RSViewUserAccount.cs
@@ -9,14 +9,18 @@ namespace MPT.RSView.UserAccount
 {
     public class RSViewUserAccount
     {
-        public string AccountId { get; set; }
+        public class PassHashPair
+        {
+            public static readonly byte[] EmptyHash = { 0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69 };
+            public string Password;
+            public byte[] Hash;
+        }
 
+        public string AccountId { get; set; }
         public ushort PasswordLength { get; set; }
         public byte[] PasswordHash { get; set; }
-        public List<byte[]> PasswordHashView
+        public List<byte[]> GetPasswordHashView()
         {
-            get
-            {
                 return new List<byte[]>()
                 {
                     PasswordHash.Skip(8*0).Take(8).ToArray(),
@@ -24,16 +28,12 @@ namespace MPT.RSView.UserAccount
                     PasswordHash.Skip(8*2).Take(8).ToArray(),
                     PasswordHash.Skip(8*3).Take(8).ToArray(),
                 };
-            }
         }
 
         public string Password { get; set; }
-        public string[] PasswordView
+        public string[] GetPasswordView()
         {
-            get
-            {
-                return PasswordHashView.Select(x => BitConverter.ToString(x).Replace('-',' ')).ToArray();
-            }
+            return GetPasswordHashView().Select(x => BitConverter.ToString(x).Replace('-', ' ')).ToArray();
         }
 
         public string LoginMacro { get; set; }
@@ -42,7 +42,6 @@ namespace MPT.RSView.UserAccount
 
         public static RSViewUserAccount Deserealize(byte[] bytes)
         {
-
             using (var reader = new BinaryReader(new MemoryStream(bytes), Encoding.Unicode))
             {
                 var account = new RSViewUserAccount();
@@ -67,76 +66,15 @@ namespace MPT.RSView.UserAccount
                     var logoutMacro = reader.ReadChars(logoutMacroLen);
                     account.LogoutMacro = new string(logoutMacro);
                 }
-
                 account.SecirityCodes = new BitArray(reader.ReadBytes(2));
 
                 var dump2 = reader.ReadUInt16();
                 if (dump2 != 0)
-                    throw new Exception("Parce error: DUMP2 after id");
-
+                    throw new Exception("Deserealize error: DUMP2 after id");
                 return account;
             }
         }
     }
 
-    public class PassHashPair
-    {
-        public readonly static byte[] EmptyHash = { 0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69 };
 
-        public string Password;
-        public byte[] Hash;
-
-        public readonly static List<PassHashPair> testPassHashList = new List<PassHashPair>()
-        {
-            new PassHashPair {
-                Password = "",
-                Hash = new byte[]
-                {
-                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69,
-                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69,
-                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69,
-                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69
-                } },
-
-            new PassHashPair {
-                Password = "1",
-                Hash = new byte[]
-                {
-                        0xdc, 0xe8, 0xc8, 0x1e, 0x64, 0x94, 0x9f, 0x4e,
-                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69,
-                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69,
-                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69
-                } },
-
-            new PassHashPair {
-                Password = "2",
-                Hash = new byte[]
-                {
-                        0x32, 0x51, 0xc6, 0x21, 0xd3, 0x1a, 0xfe, 0xdd,
-                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69,
-                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69,
-                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69,
-                } },
-
-            new PassHashPair {
-                Password = "1111",
-                Hash = new byte[]
-                {
-                        0x5f, 0x7d, 0x04, 0xcf, 0x53, 0x7c, 0x20, 0xbb,
-                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69,
-                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69,
-                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69
-                } },
-
-            new PassHashPair {
-                Password  = "11111",
-                Hash = new byte[]
-                {
-                        0x5f, 0x7d, 0x04, 0xcf, 0x53, 0x7c, 0x20, 0xbb,
-                        0xdc, 0xe8, 0xc8, 0x1e, 0x64, 0x94, 0x9f, 0x4e,
-                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69,
-                        0x44, 0x94, 0x34, 0x2b, 0x7c, 0x8f, 0x27, 0x69,
-                } },
-        };
-    }
 }
